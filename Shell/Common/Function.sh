f8582b983a4bff1fa30eb41c1a44085846d8df8d
ZhangTianJie <ztj1993@gmail.com>
2018-11-07 01:28:20 +0800

    增加了 获取平台类型函数

--- Shell/Common/Function.sh
+++ Shell/Common/Function.sh
@@ -29,3 +29,43 @@ function getStringArrayItem () {
     Arr=(${Str//$Delimiter/ })
     echo ${Arr[$Index]}
 }
+
+###############
+# Name: 获取平台类型
+# author: ZhangTianJie
+# email: ztj1993@gmail.com
+###############
+function getPlatformType () {
+    # ARM 处理器
+    if uname -m | grep -Eqi "armv7"; then
+        echo "arm armv7"
+        return 0
+    elif uname -m | grep -Eqi "armv8"; then
+        echo "arm64 armv8"
+        return 0
+    fi
+    # 是否 64 位系统
+    if [ $(getconf WORD_BIT) == "32" ] && [ $(getconf LONG_BIT) == "64" ]; then
+        is_64bit=y
+    else
+        is_64bit=n
+    fi
+    # 是否为 ARM
+    if uname -m | grep -Eqi "arm|aarch64"; then
+        is_arm=y
+    fi
+    # 开始组装合适的返回码
+    if [ "${is_arm}" == "y" ]; then
+        if [ "${is_64bit}" == "y" ]; then
+            echo "arm64"
+        else
+            echo "arm"
+        fi
+    else
+        if [ "${is_64bit}" == "y" ]; then
+            echo "amd64 x64 x86_64 x86-64"
+        else
+            echo "386 x86 i386"
+        fi
+    fi
+}
