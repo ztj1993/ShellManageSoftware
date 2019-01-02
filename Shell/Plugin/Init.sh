3b55398abe1f84b4b99a1e474e21c2b0bafc83b9
ZhangTianJie <ztj1993@gmail.com>
2019-01-02 22:43:18 +0800

    优化了 用户创建功能

--- Shell/Plugin/Init.sh
+++ Shell/Plugin/Init.sh
@@ -31,12 +31,31 @@ function MakeSoftwareConfigFile_init() {
     [ -f ${TplFileInitExtend} ] && cat ${TplFileInitExtend} | tee -a ${SoftwareConfigFile}
 }
 
+### 创建用户
+function CreateUser_init() {
+    # 创建用户组
+    groupadd -f ${SoftwareUserGroup}
+    # 判断用户是否存在
+    id -u ${SoftwareUserName} > /dev/null 2>&1
+    # 创建用户
+    if [ $? -ne 0 ]; then
+        mkdir -p ${SoftwareUserHome}
+        useradd -g ${SoftwareUserGroup} -d ${SoftwareUserHome} ${SoftwareUserName} > /dev/null 2>&1
+        [ $? -ne 0 ] && echo ">>>>> Error: create user <${SoftwareUserName}> failed" && exit 1
+    fi
+    # 复制用户环境
+    if [ ! -f ${SoftwareUserHome}/.bashrc ]; then
+        cp -frn /etc/skel/. ${SoftwareUserHome}
+    fi
+    # 配置权限
+    chown -R ${SoftwareUserName}.${SoftwareUserGroup} ${SoftwareUserHome}
+    chmod -R 700 ${SoftwareUserHome}
+}
+
 ### 默认初始化软件流程
 function InitSoftware_default(){
     # 创建用户
-    groupadd -f ${SoftwareUserGroup}
-    id -u ${SoftwareUserName} > /dev/null 2>&1
-    [ $? -ne 0 ] && useradd -m -g ${SoftwareUserGroup} -d ${SoftwareUserHome} ${SoftwareUserName}
+    CreateUser_init
     # 创建配置文件目录
     mkdir -p $(dirname ${SoftwareConfigFile})
     # 创建数据目录
